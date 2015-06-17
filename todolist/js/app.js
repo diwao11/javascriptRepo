d95c637b2f29d10fb71345bdee447b7d1e7f2afb
Dan Iwao <diwao11@gmail.com>
2015-06-17 11:21:45 -0700

    added function breakdown for problems and also added variables that we'll need to use to perform the tasks that we want done in functions. also using DOM objects to help get id's and tag names

--- todolist/js/app.js
+++ todolist/js/app.js
@@ -1,7 +1,12 @@
 //PROBLEM: user interaction doesn't provide desired results
 //SOLUTION: add interactivity so the user can manage daily tasks.
+var taskInput = document.getElementById('new-task'); //new-task
+var addButton = document.getElementsByTagName('button')[0]; //first button. Selecting the first button b/c getElementsByTagName returns an array. And we want to choose the first button.
+var incompleteTasksHolder = document.getElementById('incomplete-tasks'); //incomplete-task
+var completedTasksHolder = document.getElementById('completed-tasks'); //completed tasks
 
 //Add a new task
+var addTask = function() {
 	//When button is pressed, 
 	//Create a new list item with the text from the #new-task
 	//input (checkbox)
@@ -10,8 +15,9 @@
 	//button.edit
 	//button.delete
 	//each of these elements need to be modified and appended.
-
+}
 //Edit an existing task.
+var editTask = function(){
 	//When the EDIT button is pressed.
 		//if the parent has the class .editMode
 			//switch from .editMode
@@ -20,17 +26,22 @@
 			//switch to editMode
 			//input value becomes the labels text
 		//Toggle .editMode on the parent
+}
 
 //Delete an existing task.
+var deleteTask = function(){
 	//when delete button is pressed
 		//remove the parent list item from the ul
+}
 
-
+var taskComplete = function(){
 //Mark a task as complete
 	//when the checkbox is checked
 		//append the task list item to the #completed-tasks
+}
 
-
+var taskIncomplete = function(){
 //Mark a task as incomplete
 	//when the checkbox is unchecked
 		//append it to #incomplete-tasks
+}
