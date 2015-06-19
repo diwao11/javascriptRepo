90d8c87e9532d35313ac2626181830435f4c2350
Dan Iwao <diwao11@gmail.com>
2015-06-19 11:16:13 -0700

    datatype string lesson

---
+++ dataTypeString/js/main.js
@@ -0,0 +1,9 @@
+var username;
+var message;
+username = 'Holly';
+message = "See all our websites we've made."
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+var elNote = document.getElementById('note');
+elNote.textContent = message;
