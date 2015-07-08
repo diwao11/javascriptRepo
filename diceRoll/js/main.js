2cc6503465f5cdd6797b251a150a03a934257ead
Dan Iwao <diwao11@gmail.com>
2015-07-08 11:32:21 -0700

    added click event on button for dice roll

--- diceRoll/js/main.js
+++ diceRoll/js/main.js
@@ -2,14 +2,15 @@ var dice = {
 	sides: 6,
 	roll: function () {		//object is roll and adding an anonymous function().
 	var randonNumber = Math.floor(Math.random() * this.sides) + 1;		//'this' is the owner of the function, where the object of the method is called.
-	console.log(randonNumber);
+	// console.log(randonNumber);
+	return randonNumber;
 	}
 }
 
-var dice10 = {
-	sides: 10,
-	roll: function () {		//object is roll and adding an anonymous function().
-	var randonNumber = Math.floor(Math.random() * this.sides) + 1;		//'this' is the owner of the function, where the object of the method is called.
-	console.log(randonNumber);
-	}
-}
+// var dice10 = {
+// 	sides: 10,
+// 	roll: function () {		//object is roll and adding an anonymous function().
+// 	var randonNumber = Math.floor(Math.random() * this.sides) + 1;		//'this' is the owner of the function, where the object of the method is called.
+// 	console.log(randonNumber);
+// 	}
+// }
