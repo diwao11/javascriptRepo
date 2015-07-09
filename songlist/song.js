dab7e876da057b45e6f023b55c8bc059812d1410
Dan Iwao <diwao11@gmail.com>
2015-07-09 12:55:10 -0700

    adding comments to song.js

--- songlist/song.js
+++ songlist/song.js
@@ -1,21 +1,21 @@
-function Song(title, artist, duration) {
+function Song(title, artist, duration) {				//constructor function for song objet.
 	this.title = title;
 	this.artist = artist;
 	this.duration = duration;
 	this.isPlaying = false;
 }
 
-Song.prototype.play = function() {
-	this.isPlaying = true;
+Song.prototype.play = function() {				//prototype that song object has access to when it needs it.
+	this.isPlaying = true;									//setting this.isPlaying = true bc set to false in constructor.
 };
 
 Song.prototype.stop = function() {
 	this.isPlaying = false;
 };
 
-Song.prototype.toHTML = function() {
+Song.prototype.toHTML = function() {			//creating a prototype to replace HTML
 	var htmlString = '<li';
-	if(this.isPlaying){
+	if(this.isPlaying){											//logic for if this isPLaying equals true or false, then the htmlString will appear
 		htmlString +=' class="current"';
 	}
 	htmlString += '>';
@@ -25,5 +25,5 @@ Song.prototype.toHTML = function() {
 	htmlString += '<span class="duration">'
 	htmlString += '</span></li>';
 
-	return htmlString;
+	return htmlString;											//returning the value and it'll be called later.
 };
