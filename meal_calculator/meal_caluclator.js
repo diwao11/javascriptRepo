6b9f2d229415383f8d31801236a6a333a37496c4
Dan Iwao <diwao11@gmail.com>
2015-07-21 22:29:23 -0700

    fixing meal calculator

--- meal_calculator/meal_caluclator.js
+++ meal_calculator/meal_caluclator.js
@@ -1,30 +1,34 @@
-var Diner = function(meal, tax, tip){
+var Diner = function(meal){
 	this.meal = meal;
-	this.tax = 3;
-	this.tip = tip;
+	
 };	//closing the Diner Constructor object.
 
-Diner.prototype.dinner = function(length){
-	var dinner = "";
-	for (var i = 0; i < length; i++){
-		dinner += this.meal[i % this.meal.length] + " ";
-	}	//closing of for loop
-	console.log(dinner);
+Diner.prototype.tax = function(){
+	// var dinner = "";
+	// for (var i = 0; i < length; i++){
+	// 	dinner += this.meal[i] + " ";
+	// }	//closing of for loop
+	var tax = this.meal * 1.09;
+	return tax * this.meal;
 };	//closing the Diner Prototype dinner Method.	
 
 
-var adultDiner = function(){
-	Diner.call(this, 10);
-};
-adultDiner.prototype = Object.create(Diner.prototype);
-adultDiner.prototype.constructor = adultDiner;
+var dan = new Diner(10);
+console.log(dan);
 
-var teenageDiner = function(){
-	Diner.call(this, 5);
-};
-teenageDiner.prototype = Object.create(Diner.prototype);
-teenageDiner.prototype.constructor = teenageDiner;
 
-var dan = new adultDiner();
-dan.dinner();
+// var adultDiner = function(){
+// 	Diner.call(this, [10, 10, 5]);
+// };
+// adultDiner.prototype = Object.create(Diner.prototype);
+// adultDiner.prototype.constructor = adultDiner;
+
+// var teenageDiner = function(){
+// 	Diner.call(this, 5);
+// };
+// teenageDiner.prototype = Object.create(Diner.prototype);
+// teenageDiner.prototype.constructor = teenageDiner;
+
+// var dan = new adultDiner();
+// dan.dinner();
 
